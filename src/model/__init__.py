fb0e4bf7e9b125f38f3f7681e7f1271969b6f1f8
bekoev.maxim@gmail.com <bekoev.maxim@gmail.com>
2026-04-17 01:44:33 +0300

    add hifi++

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -2,10 +2,16 @@ from src.model.hifigan_model import HiFiGAN
 from src.model.hifigan_model_with_mrf import HiFiGANWithMRF
 from src.model.hifi_plus_plus import HiFiPlusPlusGAN
 from src.model.melspec import MelSpectrogram
+from src.model.aero import Aero
+from src.model.ap_bwe import APBWE
+from src.model.hifi import HiFi_plus_plus
 
 __all__ = [
     'MelSpectrogram',
     'HiFiGAN',
     'HiFiGANWithMRF',
     'HiFiPlusPlusGAN',
+    "Aero",
+    "APBWE",
+    "HiFi_plus_plus"
 ]
